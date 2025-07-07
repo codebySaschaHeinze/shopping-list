f7bd4d703108337591930eb44937ae740954f580
Sascha Heinze <sascha.heinze.dev@gmail.com>
2025-07-07 19:22:10 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,76 @@
+// Notizen im Browser anzeigen lassen
+
+let notes = [
+  "Gurke",
+  "Zwiebeln",
+  "Pfanne",
+  "Chips",
+  "Tampons",
+  "Zwirn",
+  "Schnürsenkel",
+  "Mehl",
+  "Kartoffeln",
+  "Pflaster",
+];
+
+let trashNotes = [];
+
+function renderNotes() {
+  let contentRef = document.getElementById("content1");
+
+  contentRef.innerText = "";
+
+  for (let indexNote = 0; indexNote < notes.length; indexNote++) {
+    contentRef.innerHTML += getNoteTemplate(indexNote);
+  }
+}
+
+function getNoteTemplate(indexNote) {
+  return /*html*/ `<div class="firstNoteContainer"><p>+ ${notes[indexNote]}</p><button onclick="deleteNote(${indexNote})" class="deleteButton">X</button></div>`;
+}
+
+function addNote() {
+  let noteInputRef = document.getElementById("noteInputBox");
+
+  let noteInput = noteInputRef.value;
+
+  notes.push(noteInput);
+
+  renderNotes();
+
+  noteInputRef.value = "";
+}
+
+function deleteNote(indexNote) {
+  let trashNote = notes.splice(indexNote, 1);
+
+  trashNotes.push(trashNote);
+
+  renderNotes();
+  renderTrashNotes();
+}
+
+function renderTrashNotes() {
+  let trashContentRef = document.getElementById("trashContent");
+
+  trashContentRef.innerText = "";
+
+  for (
+    let indexTrashNote = 0;
+    indexTrashNote < trashNotes.length;
+    indexTrashNote++
+  ) {
+    trashContentRef.innerHTML = getTrashNoteTemplate(indexTrashNote);
+  }
+}
+function getTrashNoteTemplate(indexTrashNote) {
+  return /*html*/ `<div class="trashNoteContainer"><p>+ ${trashNotes[indexTrashNote]}</p><button onclick="deleteFinallyNote(${indexTrashNote})" class="deleteFinallyButton">X</button></div>`;
+}
+
+function deleteFinallyNote(indexTrashNote) {
+  let trashNote = trashNotes.splice(indexTrashNote, 1);
+
+  trashNotes.slice(trashNote, 1);
+
+  renderTrashNotes();
+}
