81a146a3e1dd51755032b32ffbcd5c606190e185
Sascha Heinze <sascha.heinze.dev@gmail.com>
2025-07-08 16:43:43 +0200

    works, but without local storage

--- script.js
+++ script.js
@@ -1,58 +1,70 @@
 // Notizen im Browser anzeigen lassen
 
-let notes = [
-  "Kaffee",
-  "Würstchen",
-  "Taschentücher",
-  "Chips",
-  "Tastatur",
-  "Tampons",
-  "Schnürsenkel",
-];
+let notes = ["Kaffee", "Würstchen", "Apfel", "Chips", "Tastatur", "Mango"];
 
+let amounts = ["1", "6", "2", "1", "1", "2"];
 let trashNotes = [];
+let trashAmounts = [];
 
+// Notizen rendern (Liste)
 function renderNotes() {
   let contentRef = document.getElementById("content1");
 
-  contentRef.innerText = "";
+  contentRef.innerHTML = "";
 
   for (let indexNote = 0; indexNote < notes.length; indexNote++) {
     contentRef.innerHTML += getNoteTemplate(indexNote);
   }
 }
-function getNoteTemplate(indexNote) {
-  return /*html*/ `<div class="firstNoteContainer"><p>${notes[indexNote]}</p><button onclick="checkNote(${indexNote})" class="deleteButton">&#x2714;</button></div>`;
-}
 
-function addNote() {
+function getNoteTemplate(indexNote) {
+  return /*html*/ `<div class="firstNoteContainer"><p>${amounts[indexNote]} x ${notes[indexNote]}</p><button onclick="checkNote(${indexNote})" class="deleteButton">&#x2714;</button></div>`;
+}
+// Notiz und Menge hinzufügen
+function addNoteAndAmount() {
   let noteInputRef = document.getElementById("noteInputBox");
   let noteInput = noteInputRef.value;
 
-  if (!isNaN(noteInput) && noteInput !== " ") {
-    textIfNotAString();
-  } else {
-    notes.push(noteInput);
-    renderNotes();
-    noteInputRef.value = "";
+  let noteAmountRef = document.getElementById("amountInputBox");
+  let noteAmount = noteAmountRef.value;
+
+  if (!noteInput || noteAmount < 1 || isNaN(noteAmount)) {
+    textIfCorrect();
+    return;
   }
+  notes.push(noteInput);
+  amounts.push(noteAmount);
+  renderNotes();
+  noteInputRef.value = "";
+  noteAmountRef.value = "";
 }
 
-function textIfNotAString() {
-  let notAString = document.getElementById("inputAndAddNoteButton");
-  notAString.innerHTML = /*html*/ `<input
+// Validierung ob korrekte Eingabe
+function textIfCorrect() {
+  let correctValidation = document.getElementById("inputAndAddNoteButton");
+  correctValidation.innerHTML = /*html*/ `<input
         id="noteInputBox"
         type="text"
-        placeholder="Was brauchen wir noch"
+        
+        placeholder="Ich brauche..."
       />
-      <button onclick="addNote()">Liste erweitern</button>`;
+      <input
+        class="amountInputBox"
+        id="amountInputBox"
+        type="number"
+        max="50"
+        
+        placeholder="Menge"
+      />
+      <button onclick="addNoteAndAmount()">dazu</button>`;
 }
 
 function checkNote(indexNote) {
   let trashNote = notes.splice(indexNote, 1);
+  let trashAmount = amounts.splice(indexNote, 1);
 
   trashNotes.push(trashNote);
-
+  trashAmounts.push(trashAmount);
   renderNotes();
   renderTrashNotes();
 }
@@ -71,8 +83,8 @@ function renderTrashNotes() {
   }
 }
 function getTrashNoteTemplate(indexTrashNote) {
-  return /*html*/ `<div class="trashNoteContainer"><button onclick="getNoteBack()" class="getNoteBackButton">&#x21BA;</button>
-  <p>${trashNotes[indexTrashNote]}</p>
+  return /*html*/ `<div class="trashNoteContainer"><button onclick="getNoteBack(${indexTrashNote})" class="getNoteBackButton">&#x21BA;</button>
+  <p>${trashAmounts[indexTrashNote]} x ${trashNotes[indexTrashNote]}</p>
   
   <button
     onclick="deleteFinallyNote(${indexTrashNote})"
@@ -85,15 +97,13 @@ function getTrashNoteTemplate(indexTrashNote) {
 
 function deleteFinallyNote(indexTrashNote) {
   trashNotes.splice(indexTrashNote, 1);
-
+  trashAmounts.splice(indexTrashNote, 1);
   renderTrashNotes();
 }
 
-// muss noch
-
 function getNoteBack(indexTrashNote) {
   notes.push(trashNotes.splice(indexTrashNote, 1)[0]);
-
+  amounts.push(trashAmounts.splice(indexTrashNote, 1)[0]);
   renderNotes();
   renderTrashNotes();
 }
